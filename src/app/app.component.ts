aabad56e425e048188343132a6dcee62df3459a7
unknown <jerson_carranza2@hotmail.com>
2020-01-26 19:31:02 -0500

    alertifyjs-Angular8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as alertify from 'alertifyjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alertifyjs';
+
+
+
+  ejecutar(){
+    alertify.alert('Alert Message!', function(){ alertify.success('Ok'); });
+  }
+
+  ejecutar2(){
+    alertify.confirm('titulo','texto titlo',
+    ()=> {console.log('funcion ok');},
+    ()=> { alertify.error('Cancelar')}).set('labels',{ok:'ok', cancel:'cancelar'}) ;
+
+
+
+  }
+
 }
